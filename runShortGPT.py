0d24cc121c19cfe531b75076e5c94a5f759e6670
Krystian123q <80599699+Krystian123q@users.noreply.github.com>
2025-05-23 21:44:53 +0200

    Update runShortGPT.py

--- runShortGPT.py
+++ runShortGPT.py
@@ -1,4 +1,10 @@
 from gui.gui_gradio import ShortGptUI
 
 app = ShortGptUI(colab=False)
-app.launch()
+app.launch(
+    server_port=31415,
+    height=1000,
+    allowed_paths=["public/", "videos/", "fonts/"],
+    share=True,        # <-- TO JEST NAJWAŻNIEJSZE!
+    server_name="0.0.0.0"
+)
