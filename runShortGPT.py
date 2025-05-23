2b508f943b0c6cd72a18323c2c56ba7874068da8
Krystian123q <80599699+Krystian123q@users.noreply.github.com>
2025-05-23 22:55:25 +0200

    Update runShortGPT.py

--- runShortGPT.py
+++ runShortGPT.py
@@ -1,7 +1,4 @@
 from gui.gui_gradio import ShortGptUI
 
 app = ShortGptUI(colab=False)
-app.launch(
-    share=True,         # To jest kluczowe na Railway!
-    server_name="0.0.0.0"
-)
+app.launch()
